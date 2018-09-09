b13c948ac393218c9cf57102acc43f842a012170
Assassinkin <iuns@outlook.fr>
2018-09-09 18:51:44 +0100

    Adding transaction to transactions pools

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,17 +1,21 @@
 class TransactionPool {
   constructor() {
-    this.transaction = [];
+    this.transactions = [];
   }
 
   updateOrAddTransaction(transaction) {
     // look for all transaction in th epool and find the one with the same id as the one in the argument
-    let transactionWithId = this.transaction.find(t => t.id === transaction.id);
+    let transactionWithId = this.transactions.find(t => t.id === transaction.id);
 
     if (transactionWithId) {
-      this.transaction[this.transaction.indexOf(transactionWithId)] = transaction;
+      this.transactions[this.transactions.indexOf(transactionWithId)] = transaction;
     } else {
-      this.transaction.push(transaction);
+      this.transactions.push(transaction);
     }
+  }
+
+  existingTransaction(address) {
+    return this.transactions.find(t => t.input.address === address);
   }
 }
 
